05e032a34085405fde23859dad6f57f957480473
Dimitri Lahaye <contact@dimitrilahaye.net>
2020-06-19 15:32:08 +0200

    use https instead of http

--- src/openweathermap.js
+++ src/openweathermap.js
@@ -1,8 +1,8 @@
 const config = {
   urls: {
-    api: 'http://api.openweathermap.org/data/2.5/weather',
+    api: 'https://api.openweathermap.org/data/2.5/weather',
     data: 'https://openweathermap.org/data/2.5/find',
-    icon: 'http://openweathermap.org/img/w/'
+    icon: 'https://openweathermap.org/img/w/'
   },
   token: '29751b4e58b0329b2a08347d8ac9ad65'
 };
